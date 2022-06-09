1e43574f61bbea883ee323a7d1c559a89e525412
sinemorzambak <54941120+sinemorzambak@users.noreply.github.com>
2022-06-09 04:59:05 +0300

    Updates

---
+++ precache-manifest.af4ff9952c60e9f2761614486f0aef2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22197bf2438cd85a7691369c63af634",
+    "url": "/adres-defterim/index.html"
+  },
+  {
+    "revision": "877b0987c7ba72c8a631",
+    "url": "/adres-defterim/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "af3fc1a61effc5a0c2b0",
+    "url": "/adres-defterim/static/css/main.3e4a245d.chunk.css"
+  },
+  {
+    "revision": "877b0987c7ba72c8a631",
+    "url": "/adres-defterim/static/js/2.1be79cb8.chunk.js"
+  },
+  {
+    "revision": "af3fc1a61effc5a0c2b0",
+    "url": "/adres-defterim/static/js/main.590ca7ae.chunk.js"
+  },
+  {
+    "revision": "f44297c849a9c8cdf7d5",
+    "url": "/adres-defterim/static/js/runtime~main.ad0771cd.js"
+  },
+  {
+    "revision": "a5d87150c5ce53c776f7a61541a935d3",
+    "url": "/adres-defterim/static/media/bckground.a5d87150.png"
+  }
+]);
